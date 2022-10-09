6a147827ea6745dfcbf7564abe758b277557d639
Bruno ferreira <oyasuminasai538@gmail.com>
2022-10-09 14:53:54 -0300

    adcionando os arquivos do projeto

---
+++ escola/serializer.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = ['id', 'codigo_curso', 'descricao', 'nivel']
+
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+
+
+
---
+++ escola/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os alunos e alunas """
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os cursos """
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """ Exibindo todas as matriculas """
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as Matriculas de um Aluno ou Aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+
+
+
+
+
+
+
+
